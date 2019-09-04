a7fee193993b75f54c10848cbd550a69388831a0
Juan <juanfrvera@gmail.com>
2019-09-04 17:39:46 -0300

    Carpeta Modelo y empezando clase Material

---
+++ Libreria/Modelo/Material.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Material
+{
+		private string titulo;
+		private string autores;
+		private string isbn;
+		private short edicion;
+		private string issn;
+		private string categoria;
+		private string ubicacion;
+
+		public Material()
+		{
+		}
+}
