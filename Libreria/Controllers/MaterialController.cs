8c44418a3a402c0c186014f2a622d91a5aaeb6af
Sandro Pastorini <sandropastorini@gmail.com>
2019-09-04 23:43:11 -0300

    empecé a hacer el controlador de material

---
+++ Libreria/Controllers/MaterialController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+	[Route("api/[controller]")]
+
+	//Listar materiales
+	//GET: /api/Material
+	//Consultar un material
+	//GET: /api/Material/5
+	//
+	//GET: /api/
+	//Agregar un material
+	//POST: /api/Material
+	//
+	//
+	
+}
