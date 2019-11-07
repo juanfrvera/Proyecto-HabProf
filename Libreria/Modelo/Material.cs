e7f46c1ef58a19668960b69687b584239f59e0df
Gustavo Salvi <tatosalvi@gmail.com>
2019-11-07 20:30:55 -0300

    merged from master

--- Libreria/Modelo/Material.cs
+++ Libreria/Modelo/Material.cs
@@ -2,47 +2,48 @@
 
 public class Material
 {
-		private string titulo;
-		public string Titulo
-		{
-				get => titulo;
-		}
-		private string autores;
-		private string isbn;
-		private short edicion;
-		private string issn;
-		private string categoria;
-		private string ubicacion;
-		private float precio;
-		private int stock;
-		private bool anulado;
-		private string id;
-		private bool usado;
-		public Material(string titulo)
-		{
-				this.titulo = titulo;
-				this.stock = 0;
-		}
-		public void AgregarStock(int cantidad)
-		{
-				if (cantidad < 0)
-						throw new Exception("Se trató de sumar una cantidad negativa");
-				else
-						stock = stock + cantidad;
-		}
-		public void QuitarStock(int cantidad)
-		{
-				if (cantidad > stock)
-						throw new Exception("Stock insuficiente");
-				else
-						stock = stock - cantidad;
-		}
-		public void Anular()
-		{
-				anulado = true;
-		}
-		public void CambiarUbicacion(string nuevaUbicacion)
-		{
-				this.ubicacion = nuevaUbicacion;
-		}
+    private string materialId;
+    private string titulo;
+	public string Titulo
+	{
+        get { return titulo; }
+	}
+	private string autores;
+	private string isbn;
+	private short edicion;
+	private string issn;
+	private string categoria;
+	private string ubicacion;
+	private float precio;
+	private int stock;
+	private bool activo;
+		
+	private bool usado;
+	public Material(string titulo)
+	{
+			this.titulo = titulo;
+			this.stock = 0;
+	}
+	public void AgregarStock(int cantidad)
+	{
+			if (cantidad < 0)
+					throw new Exception("Se trató de sumar una cantidad negativa");
+			else
+					stock = stock + cantidad;
+	}
+	public void QuitarStock(int cantidad)
+	{
+			if (cantidad > stock)
+					throw new Exception("Stock insuficiente");
+			else
+					stock = stock - cantidad;
+	}
+	public void Anular()
+	{
+			activo = false;
+	}
+	public void CambiarUbicacion(string nuevaUbicacion)
+	{
+			this.ubicacion = nuevaUbicacion;
+	}
 }
