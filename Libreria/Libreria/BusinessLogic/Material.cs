34bd1c3f95fe3de9c34ec56fd008c19372dbd542
sandro <sandropastorini@gmail.com>
2019-11-21 20:22:36 -0300

    mejorados accesores de material

--- Libreria/Libreria/BusinessLogic/Material.cs
+++ Libreria/Libreria/BusinessLogic/Material.cs
@@ -4,33 +4,33 @@ namespace Libreria.BusinessLogic
 {
     public class Material
     {
-        private string titulo, isbn, issn, ubicacion;
-        private short edicion, stock;
-        private int id;
-        private float precio;
-        private bool anulado, usado;
-        private Proveedor proveedor;
-        private Editorial editorial;
-        private Categoria categoria;
-        private Autor autor;
+        private string _titulo, _isbn, _issn, _ubicacion;
+        private short _edicion, _stock;
+        private int _id;
+        private float _precio;
+        private bool _anulado, _usado;
+        private Proveedor _proveedor;
+        private Editorial _editorial;
+        private Categoria _categoria;
+        private Autor _autor;
 
         // Constructor
         public Material(MaterialDto dto)
         {
-            id = dto.ID;
-            titulo = dto.Titulo;
-            isbn = dto.Isbn;
-            issn = dto.Issn;
-            ubicacion = dto.Ubicacion;
-            edicion = dto.Edicion;
-            stock = dto.Stock;
-            precio = dto.Precio;
-            anulado = dto.Anulado;
-            usado = dto.Usado;
-            proveedor = new Proveedor(dto.Proveedor);
-            editorial = new Editorial(dto.Editorial);
-            categoria = new Categoria(dto.Categoria);
-            autor = new Autor(dto.Autor);
+            _id = dto.ID;
+            _titulo = dto.Titulo;
+            _isbn = dto.Isbn;
+            _issn = dto.Issn;
+            _ubicacion = dto.Ubicacion;
+            _edicion = dto.Edicion;
+            _stock = dto.Stock;
+            _precio = dto.Precio;
+            _anulado = dto.Anulado;
+            _usado = dto.Usado;
+            _proveedor = new Proveedor(dto.Proveedor);
+            _editorial = new Editorial(dto.Editorial);
+            _categoria = new Categoria(dto.Categoria);
+            _autor = new Autor(dto.Autor);
         }
         
         // Generar DTO
@@ -38,37 +38,72 @@ namespace Libreria.BusinessLogic
         {
             MaterialDto dto = new MaterialDto();
             
-            dto.ID = id;
-            dto.Titulo = titulo;
-            dto.Isbn = isbn;
-            dto.Issn = issn;
-            dto.Ubicacion = ubicacion;
-            dto.Edicion = edicion;
-            dto.Stock = stock;
-            dto.Precio = precio;
-            dto.Anulado = anulado;
-            dto.Usado = usado;
-            dto.Proveedor = proveedor.Dto();
-            dto.Editorial = editorial.Dto();
-            dto.Categoria = categoria.Dto();
-            dto.Autor = autor.Dto();
+            dto.ID = _id;
+            dto.Titulo = _titulo;
+            dto.Isbn = _isbn;
+            dto.Issn = _issn;
+            dto.Ubicacion = _ubicacion;
+            dto.Edicion = _edicion;
+            dto.Stock = _stock;
+            dto.Precio = _precio;
+            dto.Anulado = _anulado;
+            dto.Usado = _usado;
+            dto.Proveedor = _proveedor.Dto();
+            dto.Editorial = _editorial.Dto();
+            dto.Categoria = _categoria.Dto();
+            dto.Autor = _autor.Dto();
             
             return dto;
         }
 
         public void Anular()
         {
-            anulado = true;
+            _anulado = true;
         }
 
-        public string Titulo { get; }
-        public string Isbn { get; }
-        public string Issn { get; }
-        public string Ubicacion { get; }
-        public short Edicion { get; }
-        public short Stock { get; }
-        public bool Anulado { get; }
-        public bool Usado { get; }
-        public float Precio { get; }
+        public string Titulo
+        {
+            get => _titulo;
+        }
+
+        public string Isbn
+        {
+            get => _isbn;
+        }
+
+        public string Issn
+        {
+            get => _issn;
+        }
+
+        public string Ubicacion
+        {
+            get => _ubicacion;
+        }
+
+        public short Edicion
+        {
+            get => _edicion;
+        }
+
+        public short Stock
+        {
+            get => _stock;
+        }
+
+        public bool Anulado
+        {
+            get => _anulado;
+        }
+
+        public bool Usado
+        {
+            get => _usado;
+        }
+
+        public float Precio
+        {
+            get => _precio;
+        }
     }
 }
