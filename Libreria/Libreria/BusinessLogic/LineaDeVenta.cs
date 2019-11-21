704de9e59d75eff2dc9bf76ada24f6c8b8b603b2
sandro <sandropastorini@gmail.com>
2019-11-21 20:12:41 -0300

    mejores accesores para LineaDeVenta

--- Libreria/Libreria/BusinessLogic/LineaDeVenta.cs
+++ Libreria/Libreria/BusinessLogic/LineaDeVenta.cs
@@ -4,17 +4,37 @@ namespace Libreria.BusinessLogic
 {
     public class LineaDeVenta
     {
-        public int Cantidad { get; }
-        public float Precio { get; }
-        public float Descuento { get; }
-        public Material Material { get; }
+        private int _cantidad;
+        private float _precio;
+        private float _descuento;
+        private Material _material;
 
+        public int Cantidad
+        {
+            get => _cantidad;
+        }
+
+        public float Precio
+        {
+            get => _precio;
+        }
+
+        public float Descuento
+        {
+            get => _descuento;
+        }
+
+        public Material Material
+        {
+            get => _material;
+        }
+        
         public LineaDeVenta(LineaDeVentaDto dto)
         {
-            Cantidad = dto.cantidad;
-            Precio = dto.precio;
-            Descuento = dto.descuento;
-            Material = new Material(dto.Material);
+            _cantidad = dto.Cantidad;
+            _precio = dto.Precio;
+            _descuento = dto.Descuento;
+            _material = new Material(dto.Material);
         }
     }
 }
