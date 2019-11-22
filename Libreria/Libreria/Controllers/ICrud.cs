6f0740258a5320e5773dd787f87f854939e4448b
sandro <sandropastorini@gmail.com>
2019-11-21 21:15:30 -0300

    especificada la interfaz ICrud

--- Libreria/Libreria/Controllers/ICrud.cs
+++ Libreria/Libreria/Controllers/ICrud.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using Libreria.BusinessLogic.Dto;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Libreria.Controllers
 {
     public interface ICrud
     {
-        
+        public List<DtoBase> Listar(string filtro);
+        public IActionResult Agregar(DtoBase unObjeto);
+        public IActionResult Modificar(DtoBase unObjeto);
+        public DtoBase Consultar(int id);
     }
 }
