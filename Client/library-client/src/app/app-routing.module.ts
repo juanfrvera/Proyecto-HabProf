5bd7ccbcd2093afc89a12423b4d0261865994cc6
Juan <juanfrvera@gmail.com>
2020-01-24 21:29:10 -0300

    feat: angular routing, menu, awesome icons

--- Client/library-client/src/app/app-routing.module.ts
+++ Client/library-client/src/app/app-routing.module.ts
@@ -1,8 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialsComponent } from './materials/materials.component';
+import { StartComponent } from './start/start.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: StartComponent,
+    data: {
+      title: 'Inicio'
+    }
+  },
+  {
+    path: 'materiales/:id',
+    component: MaterialsComponent,
+    data: {
+      title: 'Materiales'
+    }
+  },
+  {
+    path: 'materiales',
+    component: MaterialsComponent,
+    data: {
+      title: 'Materiales'
+    }
+  },
+  {
+    path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/library-client/src/app/app.module.ts
+++ Client/library-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MaterialsComponent } from './materials/materials.component';
+import { StartComponent } from './start/start.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MaterialsComponent,
+    StartComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
