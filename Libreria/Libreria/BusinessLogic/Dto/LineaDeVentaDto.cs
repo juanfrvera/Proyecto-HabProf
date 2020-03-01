ac836196f7d7d967805a8ff2ba25db7f8e976f58
sandro <sandropastorini@gmail.com>
2020-03-01 20:03:05 -0300

    agregados accesores

--- Libreria/Libreria/BusinessLogic/Dto/LineaDeVentaDto.cs
+++ Libreria/Libreria/BusinessLogic/Dto/LineaDeVentaDto.cs
@@ -1,9 +1,11 @@
 namespace Libreria.BusinessLogic.Dto
 {
-    public class LineaDeVentaDto
+    public class LineaDeVentaDto : DtoBase
     {
-        public int Cantidad, IdMaterial;
-        public float Precio, Descuento;
-        public MaterialDto Material;
+        public int ID { get; set; }
+        public int Cantidad { get; set; }
+        public float Precio { get; set; }
+        public float Descuento { get; set; }
+        public MaterialDto Material { get; set; }
     }
 }
