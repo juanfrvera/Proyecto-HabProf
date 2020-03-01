5e03f40d0a3a0a127aeed09365e45f711d08c9a9
sandro <sandropastorini@gmail.com>
2020-03-01 20:03:27 -0300

    agregué decoradores para routeo

--- Libreria/Libreria/Controllers/Autores.cs
+++ Libreria/Libreria/Controllers/Autores.cs
@@ -2,28 +2,34 @@ using System.Collections.Generic;
 using Libreria.BusinessLogic;
 using Libreria.BusinessLogic.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Libreria.Controllers
 {
     // TODO: implementar métodos de controlador Autores.
+    [ApiController]
+    [Route("[controller]")]
     public class Autores : Controller,ICrud<AutorDto>
     {
-        // GET
+        [HttpPut]
         public IActionResult Agregar(AutorDto unObjeto)
         {
             throw new System.NotImplementedException();
         }
 
+        [HttpPut]
         public IActionResult Modificar(AutorDto unObjeto)
         {
             throw new System.NotImplementedException();
         }
 
+        [HttpGet("autores/{filtro}")]
         public List<AutorDto> Listar(string filtro)
         {
             throw new System.NotImplementedException();
         }
 
+        [HttpGet("autores/{unId}")]
         public AutorDto Consultar(int unId)
         {
             throw new System.NotImplementedException();
