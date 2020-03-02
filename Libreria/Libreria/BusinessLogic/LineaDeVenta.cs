2a67defc3ba5000c665128f6bb9b4f86fca458c4
sandro <sandropastorini@gmail.com>
2020-03-02 01:46:17 -0300

    lineadeventa linkeado a venta para el mapeo por EF. Agregados setters.

--- Libreria/Libreria/BusinessLogic/LineaDeVenta.cs
+++ Libreria/Libreria/BusinessLogic/LineaDeVenta.cs
@@ -4,41 +4,24 @@ namespace Libreria.BusinessLogic
 {
     public class LineaDeVenta
     {
-        private int _cantidad;
-        private float _precio;
-        private float _descuento;
-        private Material _material;
-        
+
         public int VentaId { get; set; }
         
         public Venta Venta { get; set; }
 
-        public int Cantidad
-        {
-            get => _cantidad;
-        }
+        public int Cantidad { get; set; }
 
-        public float Precio
-        {
-            get => _precio;
-        }
+        public float Precio { get; set; }
+        public float Descuento { get; set; }
 
-        public float Descuento
-        {
-            get => _descuento;
-        }
-
-        public Material Material
-        {
-            get => _material;
-        }
+        public Material Material { get; set; }
         
         public LineaDeVenta(LineaDeVentaDto dto)
         {
-            _cantidad = dto.Cantidad;
-            _precio = dto.Precio;
-            _descuento = dto.Descuento;
-            _material = new Material(dto.Material);
+            Cantidad = dto.Cantidad;
+            Precio = dto.Precio;
+            Descuento = dto.Descuento;
+            Material = new Material(dto.Material);
         }
     }
 }
