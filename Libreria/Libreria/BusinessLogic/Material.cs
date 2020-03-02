a2650b0cb2c3204d8fc7688332d06a266cbb9915
sandro <sandropastorini@gmail.com>
2020-03-02 02:32:59 -0300

    refactor de material. Nuevas propiedades, Anulado pasó a ser Activo, mudando Autores a MaterialAutor

--- Libreria/Libreria/BusinessLogic/Material.cs
+++ Libreria/Libreria/BusinessLogic/Material.cs
@@ -1,41 +1,47 @@
 using System.Collections.Generic;
+using System.Linq;
 using Libreria.BusinessLogic.Dto;
 
 namespace Libreria.BusinessLogic
 {
     public class Material
     {
-        private string _titulo, _isbn, _issn, _ubicacion;
-        private short _edicion, _stock;
-        private int _id;
-        private float _precio;
-        private bool _anulado, _usado;
-        private Proveedor _proveedor;
-        private Editorial _editorial;
-        private Categoria _categoria;
-        private List<Autor> _autores;
+        public string Titulo { get; set; }
+        public string Isbn { get; set; }
+        public string Issn { get; set; }
+        public string Ubicacion { get; set; }
+        public short Edicion { get; set; }
+        public short Stock { get; set; }
+        public int ID { get; set; }
+        public float Precio { get; set; }
+        public bool Activo { get; set; }
+        public bool Usado { get; set; }
+        public Proveedor Proveedor { get; set; }
+        public Editorial Editorial { get; set; }
+        public Categoria Categoria { get; set; }
+        public List<MaterialAutor> Autores { get; set; }
 
         // Constructor
         public Material(MaterialDto dto)
         {
-            _id = dto.ID;
-            _titulo = dto.Titulo;
-            _isbn = dto.Isbn;
-            _issn = dto.Issn;
-            _ubicacion = dto.Ubicacion;
-            _edicion = dto.Edicion;
-            _stock = dto.Stock;
-            _precio = dto.Precio;
-            _anulado = dto.Anulado;
-            _usado = dto.Usado;
-            _proveedor = new Proveedor(dto.Proveedor);
-            _editorial = new Editorial(dto.Editorial);
-            _categoria = new Categoria(dto.Categoria);
-            _autores = new List<Autor>();
+            ID = dto.ID;
+            Titulo = dto.Titulo;
+            Isbn = dto.Isbn;
+            Issn = dto.Issn;
+            Ubicacion = dto.Ubicacion;
+            Edicion = dto.Edicion;
+            Stock = dto.Stock;
+            Precio = dto.Precio;
+            Activo = dto.Anulado;
+            Usado = dto.Usado;
+            Proveedor = new Proveedor(dto.Proveedor);
+            Editorial = new Editorial(dto.Editorial);
+            Categoria = new Categoria(dto.Categoria);
+            Autores = new List<MaterialAutor>();
             
             foreach (AutorDto unAutorDto in dto.Autores)
             {
-                _autores.Add(new Autor(unAutorDto));
+                Autores.Add(new MaterialAutor(this, new Autor(unAutorDto)));
             }
         }
         
@@ -44,22 +50,22 @@ namespace Libreria.BusinessLogic
         {
             MaterialDto dto = new MaterialDto();
             
-            dto.ID = _id;
-            dto.Titulo = _titulo;
-            dto.Isbn = _isbn;
-            dto.Issn = _issn;
-            dto.Ubicacion = _ubicacion;
-            dto.Edicion = _edicion;
-            dto.Stock = _stock;
-            dto.Precio = _precio;
-            dto.Anulado = _anulado;
-            dto.Usado = _usado;
-            dto.Proveedor = _proveedor.Dto();
-            dto.Editorial = _editorial.Dto();
-            dto.Categoria = _categoria.Dto();
+            dto.ID = ID;
+            dto.Titulo = Titulo;
+            dto.Isbn = Isbn;
+            dto.Issn = Issn;
+            dto.Ubicacion = Ubicacion;
+            dto.Edicion = Edicion;
+            dto.Stock = Stock;
+            dto.Precio = Precio;
+            dto.Anulado = Activo;
+            dto.Usado = Usado;
+            dto.Proveedor = Proveedor.Dto();
+            dto.Editorial = Editorial.Dto();
+            dto.Categoria = Categoria.Dto();
             dto.Autores = new List<AutorDto>();
 
-            foreach (Autor unAutor in _autores)
+            foreach (Autor unAutor in Autores.Select(ma => ma.Autor))
             {
                 dto.Autores.Add(unAutor.Dto());
             }
@@ -69,91 +75,7 @@ namespace Libreria.BusinessLogic
 
         public void Anular()
         {
-            _anulado = true;
-        }
-
-        public string Titulo
-        {
-            get => _titulo;
-            set => _titulo = value;
-        }
-
-        public string Isbn
-        {
-            get => _isbn;
-            set => _isbn = value;
-        }
-
-        public string Issn
-        {
-            get => _issn;
-            set => _issn = value;
-        }
-
-        public string Ubicacion
-        {
-            get => _ubicacion;
-            set => _ubicacion = value;
-        }
-
-        public short Edicion
-        {
-            get => _edicion;
-            set => _edicion = value;
-        }
-
-        public short Stock
-        {
-            get => _stock;
-            set => _stock = value;
-        }
-
-        public int Id
-        {
-            get => _id;
-            set => _id = value;
-        }
-
-        public float Precio
-        {
-            get => _precio;
-            set => _precio = value;
-        }
-
-        public bool Anulado
-        {
-            get => _anulado;
-            set => _anulado = value;
-        }
-
-        public bool Usado
-        {
-            get => _usado;
-            set => _usado = value;
-        }
-
-        public Proveedor Proveedor
-        {
-            get => _proveedor;
-            set => _proveedor = value;
-        }
-
-        public Editorial Editorial
-        {
-            get => _editorial;
-            set => _editorial = value;
-        }
-
-        public Categoria Categoria
-        {
-            get => _categoria;
-            set => _categoria = value;
-        }
-
-        public List<Autor> Autores
-        {
-            get => _autores;
-            set => _autores = value;
+            Activo = false;
         }
     }
 }
